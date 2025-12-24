7b0f49f559712b2c1c6236ae98905e49cdd82af1
simonemurari <simone.murari3@gmail.com>
2025-12-24 17:31:48 +0100

    [update] other stuff

--- run_5_seeds.sh
+++ run_5_seeds.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+for seed in 700 200 95 50 70
+do
+    uv run ppo.py --seed $seed --run_code "RC" --save_model
+done
+
 for seed in 50 70 95 200 700
 do
-    uv run ppo.py --seed $seed --run_code "TEST"
+    uv run h_ppo_product.py --seed $seed --run_code "_ef0_RC" --end_e 0 --save_model
 done
+
--- run_5_seeds_eval.sh
+++ run_5_seeds_eval.sh
@@ -1,22 +1,51 @@
-#!/bin/bash
+# #!/bin/bash
 
-for seed in 50 70 95 200 700
+
+# for seed in 50 70 95 200 700
+# do
+#     uv run ppo_eval.py --seed $seed --size_env 8 --run_code "RC" --n_keys 2
+# done
+# for seed in 50 70 95 200 700
+# do
+#     uv run ppo_eval.py --seed $seed --heuristic --size_env 8 --run_code "_ef0_RC" --n_keys 2
+# done
+
+
+# for seed in 50 70 95 200 700
+# do
+#     uv run ppo_eval.py --seed $seed --size_env 8 --run_code "RC" --n_keys 4
+# done
+# for seed in 50 70 95 200 700
+# do
+#     uv run ppo_eval.py --seed $seed --heuristic --size_env 8 --run_code "_ef0_RC" --n_keys 4
+# done
+
+
+# for seed in 50 70 95 200 700
+# do
+#     uv run ppo_eval.py --seed $seed --size_env 16 --run_code "RC"
+# done
+# for seed in 50 70 95 200 700
+# do
+#     uv run ppo_eval.py --seed $seed --heuristic --size_env 16 --run_code "_ef0_RC"
+# done
+
+
+# for seed in 50 70 95 200 700
+# do
+#     uv run ppo_eval.py --seed $seed --size_env 16 --run_code "RC" --n_keys 2
+# done
+# for seed in 50 70 95 200 700
+# do
+#     uv run ppo_eval.py --seed $seed --heuristic --size_env 16 --run_code "_ef0_RC" --n_keys 2
+# done
+
+
+# for seed in 50 70 95 200 700
+# do
+#     uv run ppo_eval.py --seed $seed --size_env 16 --run_code "RC" --n_keys 4
+# done
+for seed in 95 200 700
 do
-    uv run ppo_eval.py --seed $seed --run_code "TEST" --n_keys 2
-done
-for seed in 50 70 95 200 700
-do
-    uv run ppo_eval.py --seed $seed --run_code "TEST" --n_keys 4
-done
-for seed in 50 70 95 200 700
-do
-    uv run ppo_eval.py --seed $seed --run_code "TEST" --size_env 16
-done
-for seed in 50 70 95 200 700
-do
-    uv run ppo_eval.py --seed $seed --run_code "TEST" --size_env 16 --n_keys 2
-done
-for seed in 50 70 95 200 700
-do
-    uv run ppo_eval.py --seed $seed --run_code "TEST" --size_env 16 --n_keys 4
+    uv run ppo_eval.py --seed $seed --heuristic --size_env 16 --run_code "_ef0_RC" --n_keys 4
 done
